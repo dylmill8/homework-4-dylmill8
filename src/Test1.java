f208f7bc28c4ad2a1822a676fc28d59523d91117
dylmill8 <mill3170@purdue.edu>
2022-11-02 14:48:12 -0400

    Changed switch statement to else-if blocks

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 enum Operation
 {
     ADD, SUBTRACT, MULTIPLY, DIVIDE
@@ -9,24 +7,19 @@ public class Test1 {
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
 
-        switch(operation) {
-            case ADD:
-                // if operation = ADD, return the sum
-                result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
-                result = num1 * num2;
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
-                    System.out.println("Can't divide by 0");
-                } else {
-                    result = ((double)num1) / num2;
-                }
-                break;
+        if (operation == Operation.ADD) {
+            // if operation = ADD, return the sum
+            result = num1 + num2;
+        } else if (operation == Operation.SUBTRACT) {
+            result = num1 - num2;
+        } else if (operation == Operation.MULTIPLY) {
+            result = num1 * num2;
+        } else if (operation == Operation.DIVIDE) {
+            if (num2 == 0) {
+                System.out.println("Can't divide by 0");
+            } else {
+                result = ((double) num1) / num2;
+            }
         }
         return result;
     }
